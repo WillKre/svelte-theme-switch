6fdfe31331b1e56b5d006457e80e4fed562a14e9
Apswak <7396157+apswak@users.noreply.github.com>
2022-08-30 21:21:24 +0100

    feat: add persistent theme switch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import ThemeSwitch from '$lib/ThemeSwitch/ThemeSwitch.svelte';
+
+	import '../app.css';
+</script>
+
+<ThemeSwitch />
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
